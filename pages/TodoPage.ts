0387300aa948f433d6a41b9f22b60b3fe627ac89
Diego-Gomez99 <diegoalberto15299@gmail.com>
2026-09-22 16:31:41 -0600

    file: README.md added!

--- pages/TodoPage.ts
+++ pages/TodoPage.ts
@@ -1,7 +1,7 @@
 import { Page, Locator, expect } from '@playwright/test';
 
 export class TodoPage {
-  // Define los elementos como propiedades de la clase
+  // Define properties for the page and its elements
   readonly page: Page;
   readonly inputBox: Locator;
   readonly todoItems: Locator;
@@ -9,13 +9,13 @@ export class TodoPage {
 
   constructor(page: Page) {
     this.page = page;
-    // Asigna los selectores en el constructor
+    // Assign locators to the elements on the page
     this.inputBox = page.getByRole('textbox', { name: 'What needs to be done?' });
     this.todoItems = page.locator('.todo-list li');
     this.clearCompletedButton = page.getByRole('button', { name: 'Clear completed' });
   }
 
-  // Define las acciones que se pueden hacer en la página
+  // Define methods to interact with the page
   async goto() {
     await this.page.goto('https://demo.playwright.dev/todomvc/#/');
   }
